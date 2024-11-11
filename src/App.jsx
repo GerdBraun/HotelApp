f3e2b7b6475b3e83dca585bbb7abdc67780f4cdf
Gerd Braun <webmaster@your-d-sign.de>
2024-11-11 14:03:54 +0100

    added booking page, route to single room, ...

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout/>}>
     <Route index element={<Home />} />
     <Route path="/rooms" element={<Rooms />} />
+    <Route path="/rooms/:id" element={<Rooms />} />
     <Route path="/booking/:id" element={<Booking />} />
     <Route path="/contact" element={<Contact />} />
     <Route path='*' element={<NotFound />} />
