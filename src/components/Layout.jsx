11e511aa615a58470d01e32f7377b06c0c5eab72
sogol mofrad <sogolmofrad@sogols-iMac.local>
2024-11-11 14:21:35 +0100

    add footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./partials/Navbar";
+import Footer from "./partials/Footer";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
