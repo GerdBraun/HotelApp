7ca9511c4d31ede96d3e570ff5e33aa924cee35a
Gerd Braun <webmaster@your-d-sign.de>
2024-11-11 15:26:33 +0100

    added fake ty pages content

--- src/components/pages/ThankYouContact.jsx
+++ src/components/pages/ThankYouContact.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 const ThankYouContact = () => {
-  return <div>ThankYouContact</div>;
-};
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-5xl font-bold">Thank you!</h1>
+              <p className="py-6">
+                your form was sent.
+              </p>
+              <Link to="/" className="btn btn-primary">back to start</Link>
+            </div>
+          </div>
+        </div>
+      );};
 
 export default ThankYouContact;
