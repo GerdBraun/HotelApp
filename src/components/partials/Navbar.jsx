fe5391d04ba096967398123c7d5ee723df16b169
Gerd Braun <webmaster@your-d-sign.de>
2024-11-11 11:16:36 +0100

    added basic navbar

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -1,5 +1,74 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start w-full">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Rooms</a>
+              <ul className="p-2">
+                <li>
+                  <a>Room 1</a>
+                </li>
+                <li>
+                  <a>Room 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Hotel Booking App</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <details>
+              <summary>Rooms</summary>
+              <ul className="p-2">
+                <li>
+                  <a>Room 1</a>
+                </li>
+                <li>
+                  <a>Room 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
