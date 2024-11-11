63d1387ae94cd0860e2054a726622c6725f52115
Silviya <silviyamaruthikkal@gmail.com>
2024-11-11 12:22:50 +0100

    Implemented Form

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,9 @@
+import React from "react";
+import Contact from './Contact'
 const Home = () => {
-  return <div>Home</div>;
+  return <div>Home
+    <Contact/>
+  </div>;
 };
 
 export default Home;
